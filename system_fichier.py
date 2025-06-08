63aa07154f61f0a27119386578c70b2668ae104b
salimataana <ana.salimata.sanou@gmail.com>
2025-06-08 13:40:39 +0200

    Adding new file python

---
+++ system_fichier.py
@@ -0,0 +1,15 @@
+{
+  "mcpServers": {
+    "filesystem": {
+      "command": "npx",
+      "args": [
+        "-y",
+        "@modelcontextprotocol/server-filesystem",
+        "C:\\Users\\Moi\\Desktop",
+        "C:\\Users\\Moi\\Downloads",
+        "C:\\Users\\Moi\\Documents"
+      ]
+    }
+  }
+}
+
